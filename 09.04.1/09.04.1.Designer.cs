61a07a5569cd51e5a72395d15979f53142b05a9c
sofiadmytriieva-dev <sofia.dmytriieva@gmail.com>
2026-05-05 20:21:00 +0300

    Add files via upload

---
+++ 09.04.1/09.04.1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace _09._04._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(48, 208);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(224, 85);
+            this.btnProcess.TabIndex = 0;
+            this.btnProcess.Text = "Обробити дані";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(419, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(332, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Тут буде результат розрахунків";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnProcess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ 09.04.1/09.04.1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace _09._04._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    
+                    string[] lines = File.ReadAllLines(openFileDialog1.FileName);
+                    List<double> prices = new List<double>();
+
+                    
+                    foreach (string line in lines)
+                    {
+                        if (double.TryParse(line.Replace(".", ","), out double price))
+                        {
+                            prices.Add(price);
+                        }
+                    }
+
+                    if (prices.Count == 0)
+                    {
+                        MessageBox.Show("Файл порожній або не містить коректних цін.");
+                        return;
+                    }
+
+                    
+                    double[] pricesArray = prices.ToArray();
+                    double sum = pricesArray.Sum();
+                    double avg = pricesArray.Average();
+                    double max = pricesArray.Max();
+                    double min = pricesArray.Min();
+                    int expensiveItems = pricesArray.Count(p => p > 100);
+
+                   
+                    label1.Text = $"Сума: {sum:F2} грн\n" +
+                                  $"Середня: {avg:F2} грн\n" +
+                                  $"Макс: {max:F2} грн\n" +
+                                  $"Мін: {min:F2} грн\n" +
+                                  $"Дорожче 100 грн: {expensiveItems} шт.";
+
+                   
+                    File.WriteAllText("результат.txt", label1.Text);
+                    MessageBox.Show("Дані оброблено та збережено у 'результат.txt'");
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                MessageBox.Show("Помилка при роботі з файлом: " + ex.Message);
+            }
+        }
+    }
+}
