3a8d31daf23124f8b171ce104ffde6530251ba1c
sofiadmytriieva-dev <sofia.dmytriieva@gmail.com>
2026-05-05 21:40:39 +0300

    Add files via upload

---
+++ 13.04/13.04.всізавдання.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace MultiApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.rtbContent = new System.Windows.Forms.RichTextBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.btnSaveFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.gameTile_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(251, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 50);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.gameTile_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(251, 271);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 50);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.gameTile_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(435, 80);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 50);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.gameTile_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(435, 187);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 50);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.gameTile_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(80, 80);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 50);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.gameTile_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(435, 289);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 50);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.gameTile_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(80, 176);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(50, 50);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.gameTile_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(251, 176);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(50, 50);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.gameTile_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(188, 360);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(144, 29);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "Нова гра";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(675, 154);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(192, 49);
+            this.btnRun.TabIndex = 10;
+            this.btnRun.Text = "Спіймай мене!";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.MouseEnter += new System.EventHandler(this.btnRun_MouseEnter);
+            // 
+            // rtbContent
+            // 
+            this.rtbContent.Location = new System.Drawing.Point(613, 440);
+            this.rtbContent.Name = "rtbContent";
+            this.rtbContent.Size = new System.Drawing.Size(100, 96);
+            this.rtbContent.TabIndex = 11;
+            this.rtbContent.Text = "";
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(435, 553);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(200, 39);
+            this.btnOpenFile.TabIndex = 12;
+            this.btnOpenFile.Text = "Відкрити файл";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.Location = new System.Drawing.Point(701, 550);
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Size = new System.Drawing.Size(186, 42);
+            this.btnSaveFile.TabIndex = 13;
+            this.btnSaveFile.Text = "Зберегти зміни";
+            this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.btnSaveFile_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 617);
+            this.Controls.Add(this.btnSaveFile);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.rtbContent);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.RichTextBox rtbContent;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Button btnSaveFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ 13.04/13.04.всізавдання.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MultiApp
+{
+    public partial class Form1 : Form
+    {
+       
+        bool isXTurn = true;
+        int steps = 0;
+        Random rnd = new Random();
+        string currentFilePath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 1. Тікаюча кнопка
+        private void btnRun_MouseEnter(object sender, EventArgs e)
+        {
+            int maxX = this.ClientSize.Width - btnRun.Width;
+            int maxY = this.ClientSize.Height - btnRun.Height;
+            btnRun.Location = new Point(rnd.Next(0, maxX), rnd.Next(0, maxY));
+        }
+
+        //  2. Хрестики-нолики 
+        private void gameTile_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (btn.Text != "") return;
+
+            btn.Text = isXTurn ? "X" : "O";
+            steps++;
+            if (CheckWinner())
+            {
+                MessageBox.Show($"Переміг {(isXTurn ? "X" : "O")}!");
+                ResetGrid();
+            }
+            else if (steps == 9)
+            {
+                MessageBox.Show("Нічия!");
+                ResetGrid();
+            }
+            isXTurn = !isXTurn;
+        }
+
+        private bool CheckWinner()
+        {
+           
+            string[][] wins = {
+                new[] { button1.Text, button2.Text, button3.Text },
+                new[] { button4.Text, button5.Text, button6.Text },
+                new[] { button7.Text, button8.Text, button9.Text },
+                new[] { button1.Text, button4.Text, button7.Text },
+                new[] { button2.Text, button5.Text, button8.Text },
+                new[] { button3.Text, button6.Text, button9.Text },
+                new[] { button1.Text, button5.Text, button9.Text },
+                new[] { button3.Text, button5.Text, button7.Text }
+            };
+            return wins.Any(w => w[0] != "" && w[0] == w[1] && w[1] == w[2]);
+        }
+
+        private void ResetGrid()
+        {
+            steps = 0; isXTurn = true;
+            foreach (var b in new[] { button1, button2, button3, button4, button5, button6, button7, button8, button9 })
+                b.Text = "";
+        }
+
+        // 3. Робота з файлом
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                currentFilePath = openFileDialog1.FileName;
+                rtbContent.Text = File.ReadAllText(currentFilePath);
+            }
+        }
+
+        private void btnSaveFile_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(currentFilePath))
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    currentFilePath = saveFileDialog1.FileName;
+                else return;
+            }
+            File.WriteAllText(currentFilePath, rtbContent.Text);
+            MessageBox.Show("Файл оновлено!");
+        }
+    }
+}
