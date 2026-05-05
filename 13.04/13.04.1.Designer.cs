7aaf539860629af75469f7b89047c74e69897c67
sofiadmytriieva-dev <sofia.dmytriieva@gmail.com>
2026-05-05 21:00:41 +0300

    Add files via upload

---
+++ 13.04/13.04.1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace _13._04._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(97, 134);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(289, 79);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Спіймай мене!";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.MouseEnter += new System.EventHandler(this.btnRun_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRun);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ 13.04/13.04.1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _13._04._1
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+
+        
+        private void btnRun_MouseEnter(object sender, EventArgs e)
+        {
+            
+            int maxX = this.ClientSize.Width - btnRun.Width;
+            int maxY = this.ClientSize.Height - btnRun.Height;
+
+            
+            btnRun.Location = new Point(rnd.Next(0, maxX), rnd.Next(0, maxY));
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
