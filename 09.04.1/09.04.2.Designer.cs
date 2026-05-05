faf04f0241d5dc6b8c7bf5f76c98e15824fb829a
sofiadmytriieva-dev <sofia.dmytriieva@gmail.com>
2026-05-05 20:30:07 +0300

    Add files via upload

---
+++ 09.04.1/09.04.2.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace _09._04._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSearchWord = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lstLines = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // txtSearchWord
+            // 
+            this.txtSearchWord.Location = new System.Drawing.Point(86, 172);
+            this.txtSearchWord.Name = "txtSearchWord";
+            this.txtSearchWord.Size = new System.Drawing.Size(100, 31);
+            this.txtSearchWord.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(86, 106);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(231, 36);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Шукати у файлі";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(510, 93);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(250, 25);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "Результат з\'явиться тут";
+            // 
+            // lstLines
+            // 
+            this.lstLines.FormattingEnabled = true;
+            this.lstLines.ItemHeight = 25;
+            this.lstLines.Location = new System.Drawing.Point(113, 281);
+            this.lstLines.Name = "lstLines";
+            this.lstLines.Size = new System.Drawing.Size(120, 79);
+            this.lstLines.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstLines);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearchWord);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSearchWord;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.ListBox lstLines;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ 09.04.1/09.04.2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace _09._04._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string wordToFind = txtSearchWord.Text.Trim();
+
+            
+            if (string.IsNullOrWhiteSpace(wordToFind))
+            {
+                MessageBox.Show("Будь ласка, введіть слово для пошуку!");
+                return;
+            }
+
+            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    
+                    string[] lines = File.ReadAllLines(openFileDialog1.FileName);
+                    int count = 0;
+                    lstLines.Items.Clear(); 
+
+                    
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        
+                        if (lines[i].Contains(wordToFind))
+                        {
+                            count++;
+                            
+                            lstLines.Items.Add($"Рядок {i + 1}: {lines[i]}");
+                        }
+                    }
+
+                    
+                    if (count > 0)
+                    {
+                        lblResult.Text = $"Знайдено! Кількість рядків зі словом: {count}";
+                    }
+                    else
+                    {
+                        lblResult.Text = "Слово не знайдено у файлі.";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    
+                    MessageBox.Show("Помилка при читанні файлу: " + ex.Message);
+                }
+            }
+        }
+    }
+}
